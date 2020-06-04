e9486352215dcee8817d4c4d17675a96e9584a38
Fabio <fabio.meihua08@gmail.com>
2020-06-04 04:56:35 -0300

    Consulta utilizando @Query

--- src/main/java/projeto/spring/data/dao/InterfaceSrpingDataUser.java
+++ src/main/java/projeto/spring/data/dao/InterfaceSrpingDataUser.java
@@ -1,5 +1,8 @@
 package projeto.spring.data.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,8 +11,9 @@ import projeto.spring.data.model.UsuarioSpringData;
 @Repository //ativa o servico de persistencia 
 public interface InterfaceSrpingDataUser extends CrudRepository<UsuarioSpringData, Long> { //passar o tipo de dados passado por ID
 	
-	
-	
+@Query(value = "SELECT p from UsuarioSpringData p WHERE p.nome like %?1%")
+public List<UsuarioSpringData> buscaporNome (String nome);
+
 	
 
 }
--- src/test/java/projeto/spring/data/AppSpringDataTest.java
+++ src/test/java/projeto/spring/data/AppSpringDataTest.java
@@ -1,5 +1,6 @@
 package projeto.spring.data;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Test;
@@ -12,67 +13,102 @@ import projeto.spring.data.dao.InterfaceSrpingDataUser;
 import projeto.spring.data.model.UsuarioSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
+@ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" })
 public class AppSpringDataTest {
 
-	
-	@Autowired ///faz a injecao de dependencias
+	@Autowired /// faz a injecao de dependencias
 	private InterfaceSrpingDataUser interfaceSpringDataUser;
-	
-	
-	
+
 	@Test
 	public void testeConsulta() {
-		
+
 		System.out.println("Iniciou com sucesso!");
 		Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(2L);
 		System.out.println("O usuario cadastrado é " + usuarioSpringData.get().getNome());
 		System.out.println(usuarioSpringData.get().getEmail());
 		System.out.println(usuarioSpringData.get().getIdade());
-		
-		
-		
+
 	}
-	
-	
+
 	@Test
 	public void testeInsert() {
-		
+
 		UsuarioSpringData usuarioSpringData = new UsuarioSpringData();
 		usuarioSpringData.setEmail("babe@gmail.com");
 		usuarioSpringData.setIdade("34");
 		usuarioSpringData.setLogin("user");
 		usuarioSpringData.setSenha("123");
 		usuarioSpringData.setNome("Babe");
-		
+
 		interfaceSpringDataUser.save(usuarioSpringData);
-		
+
 		System.out.println("Usuarios cadastrados ---> " + interfaceSpringDataUser.count());
-		
+
 	}
-	
-	
+
 	@Test
 	public void testeConsultaTodos() {
-		
+
 		Iterable<UsuarioSpringData> lista = interfaceSpringDataUser.findAll();
-		
-		for(UsuarioSpringData usuarioSpringData : lista) {
-			
+
+		for (UsuarioSpringData usuarioSpringData : lista) {
+
 			System.out.println(usuarioSpringData.getId());
 			System.out.println(usuarioSpringData.getNome());
 			System.out.println(usuarioSpringData.getIdade());
 			System.out.println(usuarioSpringData.getLogin());
 			System.out.println(usuarioSpringData.getEmail());
 			System.out.println("-------------------------------------");
-			
-			
-			
+
 		}
+
+	}
+	
+	@Test
+	public void testeUpdate() {
 		
+	Optional<UsuarioSpringData> usuarioSpringData  = interfaceSpringDataUser.findById(3L);
+	
+	UsuarioSpringData data = usuarioSpringData.get();
+	data.setNome("Juninho Spring Data Atualizado");
+	interfaceSpringDataUser.save(data);
+	
+	System.out.println("O novo nome é ::" + usuarioSpringData.get().getNome() +" e o ID é "  +usuarioSpringData.get().getId());
+		
+	}
+	
+	@Test
+	public void testeDelete() {
+		
+		/*DUAS FORMAS DE FAZER O DELETE FORMA 1*/
+		interfaceSpringDataUser.deleteById(4L);
+		System.out.println("Deletado");
+		
+		
+		/*FORMA 2 PEGANDO A CONSULTA E DELETANDO UTILIZANDO interfaceSpringDataUser*/
+		Optional<UsuarioSpringData> usuarioSpringData  = interfaceSpringDataUser.findById(4L);
+		interfaceSpringDataUser.delete(usuarioSpringData.get());
 		
 		
 		
 	}
 	
+	
+	@Test
+	public void testeConsultaNome() {
+		
+		List<UsuarioSpringData> list = interfaceSpringDataUser.buscaporNome("Fabio");
+		for (UsuarioSpringData usuarioSpringData : list) {
+
+			System.out.println(usuarioSpringData.getId());
+			System.out.println(usuarioSpringData.getNome());
+			System.out.println(usuarioSpringData.getIdade());
+			System.out.println(usuarioSpringData.getLogin());
+			System.out.println(usuarioSpringData.getEmail());
+			System.out.println("-------------------------------------");
+
+		}
+		
+	}
+
 }
